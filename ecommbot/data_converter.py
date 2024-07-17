c88c84f2ecfef8caed99c108eee79d028e59d9fb
vivek-yogi <101085320+vivek-yogi@users.noreply.github.com>
2024-07-17 17:57:41 +0530

    Convert raw data into Astradb format

--- ecommbot/data_converter.py
+++ ecommbot/data_converter.py
@@ -2,7 +2,7 @@ import pandas as pd
 from langchain_core.documents import Document
 
 
-def dataconveter():
+def dataconverter():
     product_data = pd.read_csv(r"C:\Users\Admin\Projects\E-commerce\E-commerce-chatbot\data\flipkart_product_review.csv")
 
     data         = product_data[["product_title","review"]]
